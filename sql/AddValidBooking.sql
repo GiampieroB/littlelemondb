29a0dda3996037e69099fc4aa80d3fa26ea66adf
GiampieroB <giampiero.bellomo@gmail.com>
2024-05-25 13:23:05 +0200

    sql scripts added

---
+++ sql/AddValidBooking.sql
@@ -0,0 +1,34 @@
+DROP 
+  PROCEDURE IF EXISTS AddValidBooking;
+DELIMITER // CREATE PROCEDURE AddValidBooking(
+  booking_date DATE, table_number INT, 
+  staff_id INT
+) BEGIN START TRANSACTION;
+SELECT 
+  1 AS 'status' 
+FROM 
+  bookings 
+WHERE 
+  BookingDate = booking_date 
+  AND TableNumber = table_number 
+LIMIT 
+  1 INTO @booking_status;
+IF @booking_status = 1 THEN ROLLBACK;
+SELECT 
+  CONCAT(
+    'Table ', table_number, ' is already booked '
+  ) AS 'Booking status';
+ELSE INSERT INTO bookings (
+  BookingDate, TableNumber, StaffID
+) 
+VALUES 
+  (
+    booking_date, table_number, staff_id
+  );
+COMMIT;
+SELECT 
+  CONCAT(
+    'Table ', table_number, '  booked successfully'
+  ) AS 'Booking status';
+END IF;
+END // DELIMITER;
